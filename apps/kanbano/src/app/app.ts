9e883582ff6df679c61566757beb5e302cacaf0c
Sebastienpanda <sebastienprodev@gmail.com>
2025-12-09 22:48:02 +0100

    feat: initial project

--- apps/kanbano/src/app/app.ts
+++ apps/kanbano/src/app/app.ts
@@ -2,9 +2,23 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Home } from './shared/ui/header';
 import { Hero } from './features/hero';
+import { Demo } from './features/demo/demo';
+import { Features } from './features/features';
+import { NgxSonnerToaster } from 'ngx-sonner';
+import { Cta } from './features/cta';
+import { Footer } from './shared/ui/footer';
 
 @Component({
-    imports: [RouterModule, Home, Hero],
+    imports: [
+        RouterModule,
+        Home,
+        Hero,
+        Demo,
+        Features,
+        NgxSonnerToaster,
+        Cta,
+        Footer,
+    ],
     selector: 'app-root',
     templateUrl: './app.html',
 })
